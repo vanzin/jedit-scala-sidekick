39ae6f910b7a1eb1d852b4d808845e17ec7ebf60
Marcelo Vanzin <vanza@users.sourceforge.net>
2014-09-01 18:34:37 -0700

    Add action to clean up unused imports.
    And some infrastructure for unit tests.

---
+++ src/main/scala/sidekick/scala/ImportCleaner.scala
@@ -0,0 +1,186 @@
+//            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+//                    Version 2, December 2004
+//
+// Copyright (C) 2014 Marcelo Vanzin <vanza@users.sourceforge.net>
+//
+// Everyone is permitted to copy and distribute verbatim or modified
+// copies of this license document, and changing it is allowed as long
+// as the name is changed.
+//
+//            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+//   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+//
+//  0. You just DO WHAT THE FUCK YOU WANT TO.
+package sidekick.scala;
+
+import scala.collection.mutable
+import scala.util.Try
+
+import org.gjt.sp.jedit.View
+import org.gjt.sp.util.Log
+
+import scalariform.lexer._
+import scalariform.parser._
+
+class ImportCleaner(view: View) {
+
+  private val allImports =
+      new mutable.HashMap[String, ImportEntry]()
+  private val clauses = new mutable.ListBuffer[ImportEntry]()
+
+  def run(): Unit = Try {
+    val buffer = view.getBuffer()
+    val ast = ScalaParser.parse(buffer.getText())
+    ast.foreach(handleNode)
+
+    val unused = clauses.filter(_.hasUnused())
+    if (!unused.isEmpty) {
+      buffer.writeLock()
+      try {
+        var offset = 0
+
+        unused.foreach { entry =>
+          val clause = entry.clause
+          val start = clause.firstToken.offset
+          // +1 to cover the new line. Doesn't work with \r\n.
+          val end = clause.lastToken.offset + clause.lastToken.length + 1
+
+          buffer.remove(start + offset, end - start)
+          if (entry.hasUsed()) {
+            val replacement = makeImport(entry.clause, entry.names)
+            buffer.insert(start + offset, replacement)
+            offset += (replacement.length() - end + start)
+          } else {
+            // Clean up any leading whitespace in the line removed.
+            val text = buffer.getText()
+            val importStart = start + offset - 1
+            var previousLineEnd = importStart
+            while (previousLineEnd > 0 &&
+              text.charAt(previousLineEnd) != '\r' &&
+              text.charAt(previousLineEnd) != '\n') {
+              previousLineEnd -= 1
+            }
+            if (previousLineEnd < importStart) {
+              buffer.remove(previousLineEnd + 1,
+                importStart - previousLineEnd)
+              offset -= (importStart - previousLineEnd)
+            }
+            offset -= (end - start)
+          }
+        }
+      } catch {
+        case e: Exception =>
+          e.printStackTrace()
+          throw e
+      } finally {
+        buffer.writeUnlock()
+      }
+    }
+  }
+
+  private def handleNode(node: AstNode): Unit = node match {
+    case clause: ImportClause =>
+      addImport(clause)
+
+    case expr: ExprElement =>
+      expr.tokens.foreach(matchImports)
+      expr.immediateChildren.foreach(handleNode)
+
+    case n =>
+      n.immediateChildren.foreach(handleNode)
+  }
+
+  private def addImport(clause: ImportClause) = {
+    val names = clause.importExpr match {
+      case Expr(contents) =>
+        val name = contents.last.firstToken.text
+        if (name != "_") {
+          val map = new mutable.HashMap[String, String]()
+          map += (name -> name)
+          map
+        } else {
+          new mutable.HashMap[String, String]()
+        }
+
+      case BlockImportExpr(_, selectors) =>
+        collectNames(selectors)
+    }
+
+    if (!names.isEmpty) {
+      val entry = new ImportEntry(clause, names)
+      clauses += entry
+      entry.names.keys.foreach { x => allImports += (x -> entry) }
+    }
+  }
+
+  private def collectNames(selectors: ImportSelectors):
+    mutable.Map[String, String] = {
+    val names = new mutable.LinkedHashMap[String, String]()
+
+    val first = selectors.firstImportSelector.contents.head
+    val last = selectors.firstImportSelector.contents.last
+    names += (last.firstToken.text -> first.firstToken.text)
+
+    selectors.otherImportSelectors.foreach { case (token, expr) =>
+      names += (expr.lastToken.text -> expr.firstToken.text)
+    }
+
+    names
+  }
+
+  private def matchImports(token: Token) = {
+    val text = token.text
+    if (allImports.contains(text)) {
+      allImports(text).names -= text
+      allImports -= text
+    }
+  }
+
+  private def makeImport(clause: ImportClause,
+    unused: mutable.Map[String, String]) = {
+    val ImportClause(imp, expr, others) = clause
+    val BlockImportExpr(prefix, selectors) = expr
+    val sb = new StringBuilder()
+
+    sb.append(imp.text).append(" ")
+
+    prefix.tokens.foreach { t =>
+      sb.append(t.text)
+    }
+
+    val names = collectNames(selectors)
+      .filter { case (k, v) => !unused.contains(k) }
+
+    def mkImport(k: String, v: String, wrap: Boolean) = {
+      if (k == v) {
+        k
+      } else {
+        val imp = s"$v => $k"
+        if (wrap) s"{$imp}" else imp
+      }
+    }
+
+    val text =
+      if (names.size == 1) {
+        val (k, v) = names.head
+        mkImport(k, v, true)
+      } else {
+        val imports = names
+          .map { case (k, v) => mkImport(k, v, false) }
+          .mkString(", ")
+        s"{$imports}"
+      }
+    sb.append(text).append("\n").toString()
+  }
+
+  private class ImportEntry(val clause: ImportClause,
+    val names: mutable.Map[String, String]) {
+
+    private val originalSize = names.size
+
+    def hasUsed() = names.size != originalSize
+    def hasUnused() = !names.isEmpty
+
+  }
+
+}
---
+++ src/test/scala/sidekick/scala/ImportCleanerSuite.scala
@@ -0,0 +1,191 @@
+//            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+//                    Version 2, December 2004
+//
+// Copyright (C) 2014 Marcelo Vanzin <vanza@users.sourceforge.net>
+//
+// Everyone is permitted to copy and distribute verbatim or modified
+// copies of this license document, and changing it is allowed as long
+// as the name is changed.
+//
+//            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+//   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+//
+//  0. You just DO WHAT THE FUCK YOU WANT TO.
+package sidekick.scala;
+
+import org.gjt.sp.jedit.{Buffer, View}
+import org.mockito.Matchers._
+import org.mockito.Mockito._
+import org.mockito.invocation._
+import org.mockito.stubbing._
+import org.scalatest.FlatSpec
+
+class ImportCleanerSuite extends FlatSpec {
+
+  "The Import Cleaner" should "clean a single unused import" in {
+    val original = """
+      |import com.example.Unused
+      |
+      |class Foo {
+      |  val bar: Type = _
+      |}
+      |"""
+    val expected = """
+      |
+      |class Foo {
+      |  val bar: Type = _
+      |}
+      |"""
+    runTest(original, expected)
+  }
+
+  it should "clean multiple unused imports" in {
+    val original = """
+      |import com.example.Unused
+      |import com.example.Used
+      |import com.example.Unused2
+      |
+      |class Foo {
+      |  val bar: Used = _
+      |}
+      |"""
+    val expected = """
+      |import com.example.Used
+      |
+      |class Foo {
+      |  val bar: Used = _
+      |}
+      |"""
+    runTest(original, expected)
+  }
+
+  it should "clean imports from an import list" in {
+    val original = """
+      |import com.example.{Unused, Unused2, Used}
+      |import com.example.foo.{Other, Other2, Other3}
+      |
+      |class Foo {
+      |  val bar: Used = _
+      |  val bar2: Other = _
+      |  val bar3: Other2 = _
+      |}
+      |"""
+    val expected = """
+      |import com.example.Used
+      |import com.example.foo.{Other, Other2}
+      |
+      |class Foo {
+      |  val bar: Used = _
+      |  val bar2: Other = _
+      |  val bar3: Other2 = _
+      |}
+      |"""
+    runTest(original, expected)
+  }
+
+  it should "clean renamed imports" in {
+    val original = """
+      |import com.example.{Unused, Unused2 => UnusedTwo, Used => ItsUsed}
+      |import com.example.foo.{Other => Another, Other2, Other3 => UnusedThree}
+      |
+      |class Foo {
+      |  val bar: ItsUsed = _
+      |  val bar2: Another = _
+      |  val bar3: Other2 = _
+      |}
+      |"""
+    val expected = """
+      |import com.example.{Used => ItsUsed}
+      |import com.example.foo.{Other => Another, Other2}
+      |
+      |class Foo {
+      |  val bar: ItsUsed = _
+      |  val bar2: Another = _
+      |  val bar3: Other2 = _
+      |}
+      |"""
+    runTest(original, expected)
+  }
+
+  it should "clean non-top-level imports" in {
+    val original = """
+      |class Foo {
+      |  import com.example._
+      |  import com.example.foo.Unused1
+      |  import com.example.bar.{Used, Unused}
+      |
+      |  val bar: Used = _
+      |}
+      |"""
+    val expected = """
+      |class Foo {
+      |  import com.example._
+      |  import com.example.bar.Used
+      |
+      |  val bar: Used = _
+      |}
+      |"""
+    runTest(original, expected)
+  }
+
+  it should "handle static member access" in {
+    val original = """
+      |import com.example.Used
+      |
+      |class Foo {
+      |  val bar = Used.FIELD
+      |}
+      |"""
+    val expected = """
+      |import com.example.Used
+      |
+      |class Foo {
+      |  val bar = Used.FIELD
+      |}
+      |"""
+    runTest(original, expected)
+  }
+
+  private def runTest(original: String, expected: String) = {
+    val buffer = mockBuffer(original.stripMargin)
+    val view = mockView(buffer)
+
+    new ImportCleaner(view).run()
+    assert(buffer.getText() === expected.stripMargin)
+  }
+
+  private def mockBuffer(text: String): Buffer = {
+    val buffer = mock(classOf[Buffer])
+    val sb = new StringBuilder(text)
+    when(buffer.getText()).thenAnswer(
+      new Answer[String]() {
+        override def answer(call: InvocationOnMock) = sb.toString()
+      })
+    when(buffer.insert(anyInt(), any(classOf[String]))).thenAnswer(
+      new Answer[Unit]() {
+        override def answer(call: InvocationOnMock) = {
+          val (offset, text) =
+            (call.getArguments()(0).asInstanceOf[Int],
+             call.getArguments()(1).asInstanceOf[String])
+          sb.insert(offset, text)
+        }
+      })
+    when(buffer.remove(anyInt(), anyInt())).thenAnswer(
+      new Answer[Unit]() {
+        override def answer(call: InvocationOnMock) = {
+          val (start, len) =
+            (call.getArguments()(0).asInstanceOf[Int],
+             call.getArguments()(1).asInstanceOf[Int])
+          sb.replace(start, start + len, "")
+        }
+      })
+    buffer
+  }
+
+  private def mockView(buffer: Buffer): View = {
+    val view = mock(classOf[View])
+    when(view.getBuffer()).thenReturn(buffer)
+    view
+  }
+
+}
